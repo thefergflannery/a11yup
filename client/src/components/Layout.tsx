63d9c69a820d2b75d6e918625651470820b448fd
fergflannery <fergflannery@gmail.com>
2025-05-21 14:07:51 +0100

    chore: remove Compliance from navigation

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -5,7 +5,6 @@ import { Link, useLocation } from 'react-router-dom';
 const navigation = [
   { name: 'About', href: '#features', current: false },
   { name: 'FAQ', href: '#faq', current: false },
-  { name: 'Compliance', href: '/compliance', current: false },
 ];
 
 function classNames(...classes: string[]) {
