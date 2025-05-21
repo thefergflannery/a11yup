6ab92e4ee6f0742533a0d0fdd7a264c8204da575
fergflannery <fergflannery@gmail.com>
2025-05-21 13:54:05 +0100

    fix: move Mailchimp config to server-side and add package to client dependencies

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import mailchimp from '@mailchimp/mailchimp_marketing'
-
-mailchimp.setConfig({
-  apiKey: '5e72e52c0b955cb7c1edf5cd60bc6bdb-us21',
-  server: 'us21',
-})
 
 // https://vite.dev/config/
 export default defineConfig({
